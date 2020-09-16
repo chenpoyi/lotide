1d2b103df0d271b254897e2c14cf1c0fbed93788
Paul Chen <paulchen647@gmail.com>
2020-09-16 00:19:32 +0000

    Implemented eqArrays.js

--- eqArrays.js
+++ eqArrays.js
@@ -5,3 +5,21 @@ const assertEqual = function(actual, expected) {
     console.log(`❌ ❌ ❌ Assertion Failed: ${actual} !== ${expected}`);
   }
 };
+
+const eqArrays = function(first, second) {
+  //check if first and second arrays are of the same length
+  if (first.length !== second.length) {
+    return false;
+  }
+  for (let i = 0; i < first.length; i++) {
+    if (first[i] !== second[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2], [1, 2, 3]), false); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [1, 2, "3"]), false); // => should PASS
+assertEqual(eqArrays([1, 2, "3"], [1, 2, 3]), false); // => should PASS
