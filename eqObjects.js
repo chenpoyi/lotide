b1414def0ab6050073d039636ddd61caf24f281d
Paul Chen <paulchen647@gmail.com>
2020-09-17 00:49:11 +0000

    Finished eqObjects.js to handle primitives and arrays

--- eqObjects.js
+++ eqObjects.js
@@ -6,6 +6,21 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+const eqArrays = function(first, second) {
+  //check if first and second arrays are of the same length
+  if (first.length !== second.length) {
+    return false;
+  }
+  //Loop through each element of both arrays and compare.
+  for (let i = 0; i < first.length; i++) {
+    if (first[i] !== second[i]) {
+      return false;
+      //return false if mismatch
+    }
+  }
+  return true; //return true if all matches
+};
+
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
 const eqObjects = function(object1, object2) {
@@ -16,7 +31,14 @@ const eqObjects = function(object1, object2) {
     return false;
   }
   for (let key of keys1) {
-    if (object1[key] !== object2[key]) {
+    console.log("Array: ",Array.isArray(object1[key]));
+    if(Array.isArray(object1[key]) && Array.isArray(object2[key])){// checks if both values are arrays
+      if(eqArrays(object1[key], object2[key])){//check if 2 arrays equal
+        return true;
+      } else {
+        return false;
+      }
+    } else if (object1[key] !== object2[key]) {//return false if 2 primitives are not equal
       return false;
     }
   }
@@ -25,9 +47,17 @@ const eqObjects = function(object1, object2) {
 };
 
 //TEST CODE
+//Primitives 
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
 assertEqual(eqObjects(ab, ba), true); // => true
 
 const abc = { a: "1", b: "2", c: "3" };
 assertEqual(eqObjects(ab, abc), false);
+//Arrays
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+assertEqual(eqObjects(cd, dc), true); // => true
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+assertEqual(eqObjects(cd, cd2), false); // => false
