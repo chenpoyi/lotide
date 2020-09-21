0ca294ee8890fd73d06a1533ba6d6955cab7a900
Paul Chen <paulchen647@gmail.com>
2020-09-21 20:56:12 +0000

    Dried up and separated tests for tail.js

--- tail.js
+++ tail.js
@@ -1,16 +1,10 @@
 // FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ ✅ ✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌ ❌ ❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
+
 
 const tail = function(array) {
   if (array.length > 1) {
     const newArray = array.slice(1);
-    console.log(array);
     return newArray;
   } else {
     return [];
@@ -18,13 +12,5 @@ const tail = function(array) {
   
 };
 
-// TEST CODE
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+module.exports = tail;
 
-const result = tail(["Yoyo"]);
-console.log(result);
-
-const result2 = tail([]);
-console.log(result2);
