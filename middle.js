75ba87e35e70d63f5e9984f7770e0683011bad44
Paul Chen <paulchen647@gmail.com>
2020-09-21 21:35:57 +0000

    Refactored middle and added testing

--- middle.js
+++ middle.js
@@ -1,26 +1,4 @@
-const eqArrays = function(first, second) {
-  //check if first and second arrays are of the same length
-  if (first.length !== second.length) {
-    return false;
-  }
-  //Loop through each element of both arrays and compare.
-  for (let i = 0; i < first.length; i++) {
-    if (first[i] !== second[i]) {
-      return false;
-      //return false if mismatch
-    }
-  }
-  return true; //return true if all matches
-};
 
-const assertArraysEqual = function(actual, expected) {
-  //checks if eqArrays return true
-  if (eqArrays(actual, expected)) {
-    console.log(`✅ ✅ ✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌ ❌ ❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
 
 const middle = function(array) {
   let result = [];
@@ -36,6 +14,4 @@ const middle = function(array) {
 
 };
 
-assertArraysEqual(middle([1,2,3,4,5,6,7]), [4]);
-assertArraysEqual(middle([1,2,3,4]), [2,3]);
-assertArraysEqual(middle([]), []);
+module.exports = middle;
